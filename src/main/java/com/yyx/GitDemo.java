264bc8b8089995f636b91500a30e9c94ffaead36
yyx <yyx@atguigu.com>
2022-04-25 18:07:30 +0800

    first commit

---
+++ src/main/java/com/yyx/GitDemo.java
@@ -0,0 +1,12 @@
+package com.yyx;
+
+/**
+ * @author Mr.Yang
+ * @description 功能描述
+ * @create 2022/4/25 18:06
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
